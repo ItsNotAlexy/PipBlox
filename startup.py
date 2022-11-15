fb18c559a9aceafccea28bde2cc09c27d2106f0c
ItsNotAlexy <101402577+ItsNotAlexy@users.noreply.github.com>
2022-11-15 12:43:25 +0700

    Update startup.py

--- startup.py
+++ startup.py
@@ -2,10 +2,10 @@ from luacomplier.translator import Translator
 
 complier = Translator()
 
-with open('test.py', "r") as f:
+with open("test.py", "r") as f:
     pycode = f.read()
 
 with open("src/server/init.server.lua", "a") as f:
     f.write(complier.translate(pycode))
     f.close()
-    print('Done.')
+    print("Done.")
